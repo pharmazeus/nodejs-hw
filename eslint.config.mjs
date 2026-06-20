ca522213b2937625c29b3841028188f3b817811f
Pharma.zeus <markmaydeens@gmail.com>
2026-06-20 18:55:53 -0400

    Fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
@@ -8,10 +9,7 @@ export default [
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-      globals: {
-        console: 'readonly',
-        process: 'readonly',
-      },
+      globals: globals.node,
     },
   },
 ];

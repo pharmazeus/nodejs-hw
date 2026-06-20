2029b41ee68b6b1024fc7ed796b234f9426862ef
Pharma.zeus <markmaydeens@gmail.com>
2026-06-20 18:43:55 -0400

    Done

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+  },
+];
--- src/server.js
+++ src/server.js
@@ -1,12 +1,44 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'dotenv/config';
+import pinoHttp from 'pino-http';
 
 const app = express();
-const PORT = 3000;
+app.use(pinoHttp());
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.get('/notes', (req, res) => {
+  req.log.info('Getting all notes');
+
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  req.log.info(`Getting note with ID: ${noteId}`);
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd ? 'Server error' : err.stack,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on: ${process.env.PORT}`);
 });

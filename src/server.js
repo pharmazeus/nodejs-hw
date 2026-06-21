911b1d7c4cdb7579349a357ee87b0aeca3ab9264
Pharma.zeus <markmaydeens@gmail.com>
2026-06-21 01:21:02 -0400

    Fix

--- src/server.js
+++ src/server.js
@@ -39,6 +39,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on: ${process.env.PORT}`);
 });
